fa7d450279ef8f8f5509cf1846941fc633d5b623
taniacoder <tania.cuyamedina@gmail.com>
2016-02-25 12:16:51 -0500

    terminado

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+/* $(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 600) {
+        
+    	$("header").css("background-color", "#0dc0c0");
+    } else {
+       $("header").css("background-color", "rgba(0,0,0,0.6)");
+    }
+
+}); */
+
